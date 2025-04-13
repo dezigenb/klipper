0def3897014b262be507ddbd2602e8b51553b188
dezigenb <103154870+dezigenb@users.noreply.github.com>
2025-04-13 16:52:03 +0800

    更新 install-octopi.sh

--- scripts/install-octopi.sh
+++ scripts/install-octopi.sh
@@ -14,7 +14,6 @@ install_packages()
     # hub-ctrl
     PKGLIST="${PKGLIST} libusb-dev"
     # AVR chip installation and building
-    #PKGLIST="${PKGLIST} avrdude gcc-avr binutils-avr avr-libc"
     # ARM chip installation and building
     PKGLIST="${PKGLIST} stm32flash dfu-util libnewlib-arm-none-eabi"
     PKGLIST="${PKGLIST} gcc-arm-none-eabi binutils-arm-none-eabi libusb-1.0"
